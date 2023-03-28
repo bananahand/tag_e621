2a47eac417cbf34e00a5519baaf80bb3c578df41
bananahand <nick@ballerz.gg>
2023-03-27 18:58:41 -0700

    Update tag_e621.pl

--- tag_e621.pl
+++ tag_e621.pl
@@ -13,8 +13,8 @@
 
 #### CHANGE THESE VARIABLES
 
-my $posts_csv = "posts-2023-03-26.csv";
-my $file_path = "files/e621_images/";
+my $posts_csv = "posts-2023-03-24.csv";
+my $file_path = "files/e621_popular/2007";
 
 #### DO NOT CHANGE BELOW THIS LINE
 
@@ -39,7 +39,7 @@ print "Searching: " . $file_path . "...\n";
 my @files;
 dir($file_path)->recurse(callback => sub {
   my $file = shift;
-  if($file =~ /(\.jpg|\.png|\.gif|\.swf|\.webm)\z/) {
+  if($file =~ /(\.jpg|\.png|\.gif)\z/) {
     push @files, $file->absolute->stringify;
   }
 });
@@ -49,13 +49,18 @@ for my $file (@files) {
   print "Tagging: " . $file . "\n";
   my $base_file_name = basename($file);
   (my $file_md5 = $base_file_name) =~ s/\.[^.]+$//;
-  (my $formatted_tags = $tags{$file_md5}) =~ s/\ /,/g;
+  (my $formatted_tags = $tags{$file_md5}) =~ s/\ /; /g;
   my $exifTool = Image::ExifTool->new;
-  $exifTool->Options(IgnoreMinorErrors => '1');
-  $exifTool->SetNewValue(Keywords => "$formatted_tags");
-  $exifTool->SetNewValue(Subject => "$formatted_tags");
-  $exifTool->SetNewValue(LastKeywordIPTC => "$formatted_tags");
-  $exifTool->SetNewValue(LastKeywordXMP => "$formatted_tags");
+  if($file =~ /\.(jpg|gif)\z/) {
+    $exifTool->Options(IgnoreMinorErrors => '1', OverwriteOriginal => '1', FastScan => '5');
+    $exifTool->SetNewValue(Keywords => [$formatted_tags]);
+    $exifTool->SetNewValue(Subject => [$formatted_tags]);
+  }
+  if($file =~ /\.png\z/) {
+    $exifTool->Options(IgnoreMinorErrors => '1', OverwriteOriginal => '1', FastScan => '1');
+    $exifTool->SetNewValue(Keywords => [$formatted_tags]);
+    $exifTool->SetNewValue(Subject => [$formatted_tags]);
+  }
   $exifTool->WriteInfo($file);
 }
 
